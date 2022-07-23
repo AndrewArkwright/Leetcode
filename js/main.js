4b665b6ac8d6b59859724acf0e4dbaf2844d28cc
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-07-23 08:27:54 -0400

    Add function that takes in a Roman Numeral string and then returns it as a number

--- js/main.js
+++ js/main.js
@@ -34,3 +34,25 @@
         return true
     }
 }
+
+/**
+ * Convert Roman Numeral to Int
+ * @param {string} s
+ * @return {number}
+ * "III" => 3
+ * Easiest way to probably do it is to make a table of possible characters I would receive so I can use it to convert each character. After I convert, I would just remove it from the string and keep track of the number to return it.
+ */
+ var romanToInt = function(s) {
+    let table = {CM:900, CD:400, XC:90, XL:40, IX:9, IV:4, M:1000, D:500, C:100, L:50, X:10, V:5, I:1}
+    let number = 0, i = 0
+  
+    for (i in table) {
+    while (s.includes(i)) {
+      number += Number(table[i])
+      let ind = s.indexOf(i)
+      s = s.substring(0, ind) + s.substring(ind + i.length)
+    }
+  }
+
+   return number
+}
