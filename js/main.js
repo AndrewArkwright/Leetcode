c3fe2f7cfe7db0f55b46fde01b7a4966443ec83b
andrew arkwright <andrew.arkwright@hotmail.com>
2022-10-28 21:41:14 -0400

    Add function that when given two linked lists of single digit numbers, will add the combined numbers reverse together and return it as a single digit linked list.

--- js/main.js
+++ js/main.js
@@ -698,3 +698,35 @@ var lastRemaining = function(n) {
     }
     return first
 }
+
+/**
+ * @Description - You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * @Parameter - We are given two linked lists that are not empty and do not have leading 0s unless it is 0 itself
+ * @Return - We return a linked list starting from the head
+ * @Example - l1 = [2,4,3], l2 = [5,6,4] => [7,0,8]  (342 + 465 = 807)
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+ var addTwoNumbers = function(l1, l2) {
+    let carry = 0 //Carries over number if > 9
+    let answerTemp = new ListNode(-1)
+    let answer = answerTemp //Represents the head of the list
+  
+    while (l1 || l2 || carry) {
+      let l1Val = l1 ? l1.val : 0
+      let l2Val = l2 ? l2.val : 0
+  
+      let nextVal = (l1Val + l2Val + carry) % 10
+      carry = Math.floor((l1Val + l2Val + carry) / 10)
+      answerTemp.next = new ListNode(nextVal)
+      answerTemp = answerTemp.next
+  
+      l1 = l1 ? l1.next : null //These change the number from current to next val if it exists
+      l2 = l2 ? l2.next : null
+    }
+      return answer.next
+  }
